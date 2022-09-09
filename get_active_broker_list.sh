a299f206f42d4992a1d340780e3bcc7583fc8933
Subham Mandal <subhammandal01@gmail.com>
2022-09-10 00:09:46 +0530

    Update get_active_broker_list.sh

--- get_active_broker_list.sh
+++ get_active_broker_list.sh
@@ -9,4 +9,6 @@ if [[ ! -n $(docker images -q kafka-broker-list) ]];then
 docker build -t kafka-broker-list -f dockerfile.broker_list .
 fi
 
+printf "Getting info of number of kafka brokers in cluster.."
+echo " OK!"
 docker run --rm -it -v $(pwd):/tmp -w /tmp --network kafka_default --entrypoint bash kafka-broker-list $CMD
